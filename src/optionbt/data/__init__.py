91f4aa4e9a4ba85a349b76baa155560a751c2320
yeziyang25 <yeziyangx@gmail.com>
2026-02-18 13:43:44 -0500

    Revert "Rewrite option backtest framework: modular architecture with pluggable data sources"

--- src/optionbt/data/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Data loading and management utilities."""
-
-from src.optionbt.data.loader import DataLoader, CSVDataProvider
-
-__all__ = ["DataLoader", "CSVDataProvider"]
--- src/optionbt/engine/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Engine module for backtest execution."""
-
-from src.optionbt.engine.backtest import BacktestEngine
-
-__all__ = ["BacktestEngine"]
